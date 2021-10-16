60eb15d938719468054debba3ece68f837792d15
matthiashaefeli <haefeli.matthias2gmail.com>
2021-10-16 13:23:52 -0500

    bcrypt and jwt

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,8 +1,19 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'Please provide email and password' });
+  }
 
   User.findOne({ email: email }, function(err, existingUser) {
     if (err) { return next(err); }
@@ -19,7 +30,7 @@ exports.signup = function(req, res, next) {
     user.save(function(err) {
       if (err) { return next(err); }
 
-      res.json(user);
+      res.json({ token: tokenForUser(user) });
     });
   })
 }
